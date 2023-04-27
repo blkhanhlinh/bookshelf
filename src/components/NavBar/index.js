8922b648daa33273b6970ebc5085a8ef0c4eed85
Thanh Lan <emeraldlan1105@gmail.com>
2023-04-27 13:40:25 +0700

    modularize navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,1 +1,4 @@
 export { default as NavBar } from './NavBar'
+export { default as MainNav } from './MainNav'
+export { default as SubNav } from './SubNav'
+export { default as SearchBar } from './SearchBar'
