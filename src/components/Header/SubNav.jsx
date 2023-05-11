41b7698db0a360d2e6c090f0d78f99e7214155ba
blkhanhlinh <linhbui8103@gmail.com>
2023-05-11 14:09:32 +0700

    edit, add illustration :sparkles:

---
+++ src/components/Header/SubNav.jsx
@@ -0,0 +1,80 @@
+import { Stack } from '@chakra-ui/react'
+import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import useWindowSize from '@/utils/hooks/useWindowSize'
+
+const SubNav = () => {
+	const [isClient, setIsClient] = useState(false)
+	const { width } = useWindowSize()
+
+	useEffect(() => {
+		setIsClient(true)
+	}, [])
+
+	if (!isClient || width < 1024) {
+		return null
+	}
+
+	const categories = [
+		{
+			name: 'Home',
+			href: '/',
+		},
+		{
+			name: 'Best Sellers',
+			href: '/',
+		},
+		{
+			name: 'New Arrivals',
+			href: '/',
+		},
+		{
+			name: 'Coming Soon',
+			href: '/',
+		},
+		{
+			name: 'Fiction',
+			href: '/',
+		},
+		{
+			name: 'Business & Management',
+			href: '/',
+		},
+		{
+			name: "Children's Books",
+			href: '/',
+		},
+		{
+			name: 'Dictionaries & Languages',
+			href: '/',
+		},
+		{
+			name: 'Other Languages',
+			href: '/',
+		},
+	]
+
+	return (
+		<nav id='sub-nav'>
+			<Stack
+				className='h-12 bg-background py-3'
+				direction='row'
+				spacing='auto'
+				justify='center'
+				align='center'
+			>
+				{categories.map((category, index) => (
+					<Link
+						href={category.href}
+						key={index}
+						className='text-info text-base'
+					>
+						{category.name}
+					</Link>
+				))}
+			</Stack>
+		</nav>
+	)
+}
+
+export default SubNav
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,31 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import axios from 'axios'
-import { NavBar } from '@/components/NavBar';
+import { Header } from '@/components/Header'
+import Layout from '@/components/layout'
 
 export default function Home({ books }) {
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	return (
+		<>
+			<Head>
+				<meta charSet='utf-8' />
+				<meta	name='viewport'	content='initial-scale=1.0, width=device-width'/>
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <title>Bookshelf</title>
-      </Head>
-      <NavBar />
-      <h1>Bookshelf</h1>
-      {/*{books.map(book => (
+			</Head>
+      <Layout>
+        <Header />
+      </Layout>
+			<h1>Bookshelf</h1>
+			{/* {books.map(book => (
         <div key={book.id}>
           <h2>{book.title}</h2>
           <p>{book.author}</p>
           <Image src={book.image} alt={book.title} width={200} height={200} />
         </div>
-      ))}*/}
+      ))} */}
     </>
-  );
+  )
 }
 
 {/*export async function getServerSideProps() {
